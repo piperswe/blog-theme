ee2af2b0af4cc98b715c31225e2fef403892a6d9
Piper McCorkle <contact@piperswe.me>
2021-10-02 02:36:04 -0500

    Use brand color in code theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,6 @@ import 'katex/contrib/auto-render/auto-render';
 import 'katex/dist/katex.min.css';
 
 import Prism from 'prismjs';
-import 'prism-themes/themes/prism-synthwave84.css';
 
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'photoswipe/dist/photoswipe.css';
